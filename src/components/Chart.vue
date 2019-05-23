6b899328d4c0efef78e0b484ab59e643a8b6a820
Junting <342766475@qq.com>
2019-05-23 23:53:33 +0800

    :sparkles: 引入 echarts 图表插件库，进行封装使用

---
+++ src/components/Chart.vue
@@ -0,0 +1,55 @@
+<template>
+  <div ref="chartDom"></div>
+</template>
+
+<script>
+import echarts from "echarts";
+import debounce from "lodash/debounce"; // 防抖
+import { addListener, removeListener } from "resize-detector"; // 监听 DOM 元素, resize 监听
+
+export default {
+  props: {
+    option: {
+      type: Object,
+      default: () => {}
+    }
+  },
+  watch: {
+    option(val) {
+      this.chart.setOption(val);
+    }
+    // option: { // 深度监听引用类型属性值的变化,相当耗性能；变更新值，就不需要深度监听了
+    //   handler(val) {
+    //     this.chart.setOption(val);
+    //   },
+    //   deep: true
+    // }
+  },
+  created() {
+    this.resize = debounce(this.resize, 300); // 防抖处理
+  },
+  mounted() {
+    this.renderChart();
+    addListener(this.$refs.chartDom, this.resize);
+  },
+  beforeDestroy() {
+    removeListener(this.$refs.chartDom, this.resize); // 移除监听
+    this.chart.dispose(); // 销毁实例
+    this.chart = null; // 手动释放 this.chart
+  },
+  methods: {
+    resize() {
+      console.log("resize");
+      this.chart.resize();
+    },
+    renderChart() {
+      // 基于准备好的dom，初始化echarts实例
+      this.chart = echarts.init(this.$refs.chartDom);
+      // 绘制图表
+      this.chart.setOption(this.option);
+    }
+  }
+};
+</script>
+
+<style></style>
--- src/views/Dashboard/Analysis.vue
+++ src/views/Dashboard/Analysis.vue
@@ -1,11 +1,50 @@
 <template>
   <div>
-    Analysis
+    <Chart :option="chartOption" style="height: 400px;" />
   </div>
 </template>
 
 <script>
-export default {};
+import random from "lodash/random";
+import Chart from "@/components/Chart.vue";
+
+export default {
+  components: {
+    Chart
+  },
+  data() {
+    return {
+      chartOption: {
+        title: {
+          text: "ECharts 入门示例"
+        },
+        tooltip: {},
+        xAxis: {
+          data: ["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"]
+        },
+        yAxis: {},
+        series: [
+          {
+            name: "销量",
+            type: "bar",
+            data: [5, 20, 36, 10, 10, 20]
+          }
+        ]
+      }
+    };
+  },
+  mounted() {
+    this.intervalTimer = setInterval(() => {
+      this.chartOption.series[0].data = this.chartOption.series[0].data.map(
+        () => random(100)
+      );
+      this.chartOption = { ...this.chartOption }; // 变更新值，就不需要深度监听了
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalTimer);
+  }
+};
 </script>
 
 <style></style>
