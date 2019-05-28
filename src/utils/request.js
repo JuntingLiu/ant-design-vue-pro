f86bc8ae25c4e610b0a376efdb4b97d721803e0c
Junting <342766475@qq.com>
2019-05-28 23:07:30 +0800

    :sparkles: cross-env 多环境命令支持、axios 简单版封装、项目支持 JSX 语法

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,25 @@
 import axios from 'axios';
+import { notification } from 'ant-design-vue';
 
 function request(options) {
   return axios(options)
-    .then(response => {
-      console.log(response);
+    .then(res => {
+      return res;
     })
     .catch(error => {
-      console.log(error);
+      const {
+        response: { status, statusText }
+      } = error;
+      notification.error({
+        // eslint-disable-next-line no-unused-vars
+        message: h => (
+          <div>
+            请求错误 <span style="color: red;">{status}</span>: {options.url}
+          </div>
+        ),
+        description: statusText
+      });
+      return Promise.reject(error);
     });
 }
 
