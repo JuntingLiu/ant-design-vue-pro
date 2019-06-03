24bbd7c4a3bf3a360127728585ce8f66f3f826af
Junting <342766475@qq.com>
2019-06-04 00:45:37 +0800

    :sparkles: 表单/使用 antDesign 的表单校验方式

--- src/views/Forms/BasicForm.vue
+++ src/views/Forms/BasicForm.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a-form :layout="formLayout">
+    <a-form :layout="formLayout" :form="form">
       <a-form-item
         label="Form Layout"
         :label-col="formItemLayout.labelCol"
@@ -10,38 +10,37 @@
           default-value="horizontal"
           @change="handleFormLayoutChange"
         >
-          <a-radio-button value="horizontal">
-            Horizontal
-          </a-radio-button>
-          <a-radio-button value="vertical">
-            Vertical
-          </a-radio-button>
-          <a-radio-button value="inline">
-            Inline
-          </a-radio-button>
+          <a-radio-button value="horizontal">Horizontal</a-radio-button>
+          <a-radio-button value="vertical">Vertical</a-radio-button>
+          <a-radio-button value="inline">Inline</a-radio-button>
         </a-radio-group>
       </a-form-item>
       <a-form-item
         label="Field A"
         :label-col="formItemLayout.labelCol"
         :wrapper-col="formItemLayout.wrapperCol"
-        :validate-status="fieldAStatus"
-        :help="fieldAHelp"
-        has-feedback
       >
-        <a-input v-model="fieldA" placeholder="input placeholder" />
+        <a-input
+          v-decorator="[
+            'fieldA',
+            {
+              // 只初始化一次，后续需要修改得使用
+              initialValue: fieldA,
+              rules: [{ required: true, min: 6, message: '必须大于5个字符' }]
+            }
+          ]"
+          placeholder="input placeholder"
+        />
       </a-form-item>
       <a-form-item
         label="Field B"
         :label-col="formItemLayout.labelCol"
         :wrapper-col="formItemLayout.wrapperCol"
       >
-        <a-input v-model="fieldB" placeholder="input placeholder" />
+        <a-input v-decorator="['fieldB']" placeholder="input placeholder" />
       </a-form-item>
       <a-form-item :wrapper-col="buttonItemLayout.wrapperCol">
-        <a-button type="primary">
-          Submit
-        </a-button>
+        <a-button type="primary" @click="handelSubmit">Submit</a-button>
       </a-form-item>
     </a-form>
   </div>
@@ -52,22 +51,9 @@ export default {
   data() {
     return {
       formLayout: 'horizontal',
-      fieldA: '',
-      fieldB: '',
-      fieldAStatus: '',
-      fieldAHelp: ''
+      fieldA: 'hello',
+      fieldB: ''
     };
-  },
-  watch: {
-    fieldA(val) {
-      if (val.length <= 5) {
-        this.fieldAStatus = 'error';
-        this.fieldAHelp = '必须大于5个字符';
-      } else {
-        this.fieldAStatus = 'success';
-        this.fieldAHelp = '';
-      }
-    }
   },
   computed: {
     formItemLayout() {
@@ -88,9 +74,34 @@ export default {
         : {};
     }
   },
+  beforeCreate() {
+    // 构建一个 Form 实例
+    this.form = this.$form.createForm(this);
+  },
+  mounted() {
+    // 模拟编辑接口, 使用此方式，动态改变表单数据值
+    setTimeout(() => {
+      this.form.setFieldsValue({ fieldA: 'hello vue!' });
+    }, 3000);
+  },
   methods: {
     handleFormLayoutChange(e) {
       this.formLayout = e.target.value;
+    },
+    handelSubmit() {
+      // form 实例的 校验字段 API
+      this.form.validateFields((err, values) => {
+        if (!err) {
+          console.log('校验通过后的数据：', values);
+          // 数据通过，发送给服务端
+          // ...
+          // 成功后，数据同步到当前
+          Object.assign(this, { fieldA: 'hellossss' });
+          // this.form.setFieldsValue({ fieldA: 'hello world!' }); 也只有这种发式 DOM 上的数据值才能同步
+        } else {
+          console.warn('error', err);
+        }
+      });
     }
   }
 };
